21387bb46af5d4fb39ea570e5d85e30beb187638
Oscar Diaz <oscarock02@gmail.com>
2025-07-01 18:21:17 -0500

    :art: Mejora el test de saludo para utilizar get_json() en lugar de get_data()

--- 05-mi-app-python/tests/test_app.py
+++ 05-mi-app-python/tests/test_app.py
@@ -23,7 +23,8 @@ def test_suma(client):
 
 def test_saludo(client):
     r = client.get('/saludo/Rox')
-    assert '¡Hola Rox!' in r.get_data(as_text=True)
+    data = r.get_json()
+    assert data['saludo'] == '¡Hola Rox!'
 
 def test_multiplicar(): assert multiplicar(2, 3) == 6
 def test_es_par(): assert es_par(4)
